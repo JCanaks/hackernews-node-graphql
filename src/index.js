5ba8808cfe2f7243866cf807ce4384cdd28a15aa
JCanaks <chiomimo15@gmail.com>
2019-06-26 16:06:46 +0100

    ISSUE-#1(graphqlserver): setting up graphql server
     - setup eslint for the project
    
    [Finishes ISSUE #1]

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const resolvers = {
         info: () => 'This is the API of a Hackernews Clone'
     }
 }
+
 const server = new GraphQLServer({ typeDefs, resolvers });
 
+// eslint-disable-next-line no-console
 server.start(() => console.log('Server is running on http://localhost:4000'));
